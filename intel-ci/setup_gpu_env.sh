69197abf4b87decb1bbe4154af36137a3c7dc63d
Renato Golin <rengolin@systemcall.eu>
2025-11-28 13:00:50 +0000

    [intel-ci] Build llvm with level zero and run XeGPU matmul benchmark

---
+++ intel-ci/setup_gpu_env.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+#
+# Sets up GPU build environment.
+# Usage: source setup_gpu_env.sh
+
+# Include common utils
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+source ${SCRIPT_DIR}/common.sh
+
+# Env CUDA setup
+if [[ ${GPU,,} =~ "cuda" ]]; then
+  echo "Setting up CUDA environment"
+  echo "Hard-coding MLIR-compatible CUDA version (12.9)"
+  source /swtools/cuda/12.9.0/cuda_vars.sh
+  check_program nvcc
+fi
+
+# Env Intel setup
+if [[ ${GPU,,} =~ "intel" ]]; then
+  echo "Setting up Intel XeGPU environment"
+  VERSION="25.44.36015.5"
+  echo "Using driver version ${VERSION}"
+  source /swtools/intel-gpu/$VERSION/intel_gpu_vars.sh
+  check_program iga64
+fi
