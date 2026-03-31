libxsmm/lighthouse

commit f393521c6cdcd19031f8023d57447989a5267800

Remove lower_payload from runner (#102)
This finished the work started in #100 and #101

Code entirely done by Claude Sonnet, and verified by me one by one.

Some temp variables were needed because there was some cross-usage
between a stack payload object and the workload's function that
generates the payload.

examples/xegpu/matmul.py
@@ -21,10 +21,10 @@ import numpy as np
 from mlir import ir
 
 from lighthouse import dialects as lh_dialects
+from lighthouse.pipeline.driver import TransformDriver
 from lighthouse.execution import (
     benchmark,
     execute,
-    lower_payload,
     get_bench_wrapper_schedule,
     MemoryManager,
     GPUMemoryManager,
@@ -462,12 +462,12 @@ enabled via CLI arguments.
         )
 
         if args.dump_kernel or args.dump_schedule:
-            payload = lower_payload(
-                wload.payload_module(),
+            pipeline = TransformDriver(
                 wload.schedule_modules(
                     stop_at_stage=args.dump_kernel, parameters=params
-                ),
+                )
             )
+            payload = pipeline.apply(wload.payload_module())
             if args.dump_kernel:
                 print(payload)
             if args.dump_schedule:

examples/xegpu/mlp.py
@@ -25,10 +25,10 @@ import numpy as np
 from mlir import ir
 
 from lighthouse import dialects as lh_dialects
+from lighthouse.pipeline.driver import TransformDriver
 from lighthouse.execution import (
     benchmark,
     execute,
-    lower_payload,
     get_bench_wrapper_schedule,
     MemoryManager,
     GPUMemoryManager,
@@ -372,12 +372,12 @@ if __name__ == "__main__":
         params = parameter_selector.get_parameters_for_layers(matmuls)
 
         if args.dump_kernel or args.dump_schedule:
-            payload = lower_payload(
-                wload.payload_module(),
+            pipeline = TransformDriver(
                 wload.schedule_modules(
                     stop_at_stage=args.dump_kernel, parameters=params
-                ),
+                )
             )
+            payload = pipeline.apply(wload.payload_module())
             if args.dump_kernel:
                 print(payload)
             if args.dump_schedule:
@@ -397,9 +397,10 @@ if __name__ == "__main__":
                     memory_manager.copy(inputs[0], result_host_copy)
 
                 # Execute kernel once.
+                pipeline = TransformDriver(wload.schedule_modules(parameters=params))
+                payload = pipeline.apply(wload.payload_module())
                 execute(
-                    wload.payload_module(),
-                    schedule_modules=wload.schedule_modules(parameters=params),
+                    payload,
                     host_input_buffers=wload._initial_host_arrays,
                     mem_manager_cls=wload.memory_manager_class,
                     shared_libs=wload.shared_libs(),
@@ -420,8 +421,7 @@ if __name__ == "__main__":
                     raise ValueError("Result mismatch!")
 
             times = benchmark(
-                wload.payload_module(),
-                schedule_modules=wload.schedule_modules(parameters=params),
+                payload,
                 host_input_buffers=wload._initial_host_arrays,
                 mem_manager_cls=wload.memory_manager_class,
                 shared_libs=wload.shared_libs(),

examples/xegpu/softmax.py
@@ -14,10 +14,10 @@ import numpy as np
 from mlir import ir
 
 from lighthouse import dialects as lh_dialects
+from lighthouse.pipeline.driver import TransformDriver
 from lighthouse.execution import (
     benchmark,
     execute,
-    lower_payload,
     get_bench_wrapper_schedule,
     GPUMemoryManager,
 )
@@ -247,12 +247,12 @@ if __name__ == "__main__":
         wload = XeGPUSoftmax(M=M, N=N, dtype=dtype)
 
         if args.dump_kernel or args.dump_schedule:
-            payload = lower_payload(
-                wload.payload_module(),
+            pipeline = TransformDriver(
                 wload.schedule_modules(
                     stop_at_stage=args.dump_kernel, parameters=params
-                ),
+                )
             )
+            payload = pipeline.apply(wload.payload_module())
             if args.dump_kernel:
                 print(payload)
             if args.dump_schedule:
@@ -272,9 +272,10 @@ if __name__ == "__main__":
                     memory_manager.copy(inputs[0], result_host_copy)
 
                 # Execute kernel once.
+                pipeline = TransformDriver(wload.schedule_modules(parameters=params))
+                payload = pipeline.apply(wload.payload_module())
                 execute(
-                    wload.payload_module(),
-                    schedule_modules=wload.schedule_modules(parameters=params),
+                    payload,
                     host_input_buffers=wload._initial_host_arrays,
                     mem_manager_cls=wload.memory_manager_class,
                     shared_libs=wload.shared_libs(),
@@ -292,8 +293,7 @@ if __name__ == "__main__":
                     raise ValueError("Result mismatch!")
 
             times = benchmark(
-                wload.payload_module(),
-                schedule_modules=wload.schedule_modules(parameters=params),
+                payload,
                 host_input_buffers=wload._initial_host_arrays,
                 mem_manager_cls=wload.memory_manager_class,
                 shared_libs=wload.shared_libs(),

examples/cpu/x86/matmul.py
@@ -22,10 +22,10 @@ from mlir.dialects import linalg, transform
 from mlir.dialects.transform import tensor
 
 from lighthouse import dialects as lh_dialects
+from lighthouse.pipeline.driver import TransformDriver
 from lighthouse.execution import (
     benchmark,
     execute,
-    lower_payload,
     get_bench_wrapper_schedule,
 )
 from lighthouse.utils.numpy import numpy_to_mlir_type
@@ -361,10 +361,10 @@ if __name__ == "__main__":
         wload = Matmul(*args.sizes, dtype=in_dtype, tile_size=args.tile_size)
 
         if args.dump_kernel or args.dump_schedule:
-            payload = lower_payload(
-                wload.payload_module(),
-                wload.schedule_modules(stop_at_stage=args.dump_kernel),
+            pipeline = TransformDriver(
+                wload.schedule_modules(stop_at_stage=args.dump_kernel)
             )
+            payload = pipeline.apply(wload.payload_module())
             if args.dump_kernel:
                 print(payload)
             if args.dump_schedule:

examples/execution/example.py
@@ -19,10 +19,10 @@ from mlir.dialects import transform
 from lighthouse import dialects as lh_dialects
 from lighthouse.pipeline.helper import match
 from lighthouse.pipeline.stage import PassBundles, apply_bundle
+from lighthouse.pipeline.driver import TransformDriver
 from lighthouse.execution import (
     execute,
     benchmark,
-    lower_payload,
     get_bench_wrapper_schedule,
 )
 
@@ -140,18 +140,14 @@ if __name__ == "__main__":
         wload = ElementwiseSum(400, 400)
 
         print(" Dump kernel ".center(60, "-"))
-        payload = lower_payload(
-            wload.payload_module(),
-            wload.schedule_modules(stop_at_stage="bufferized"),
-        )
+        pipeline = TransformDriver(wload.schedule_modules(stop_at_stage="bufferized"))
+        payload = pipeline.apply(wload.payload_module())
         print(payload)
         for schedule_module in wload.schedule_modules():
             print(schedule_module)
 
-        payload = lower_payload(
-            wload.payload_module(),
-            wload.schedule_modules(),
-        )
+        pipeline = TransformDriver(wload.schedule_modules())
+        payload = pipeline.apply(wload.payload_module())
 
         print(" Execute 1 ".center(60, "-"))
         execute(

examples/feed-forward-mpi/feed-forward-mpi.py
@@ -27,10 +27,10 @@ from mlir.execution_engine import ExecutionEngine
 
 from lighthouse import dialects as lh_dialects
 from lighthouse.pipeline.helper import apply_registered_pass, match
+from lighthouse.pipeline.driver import TransformDriver
 from lighthouse.execution import (
     benchmark,
     execute,
-    lower_payload,
     get_bench_wrapper_schedule,
 )
 from lighthouse.schedule import schedule_boilerplate
@@ -428,10 +428,8 @@ if __name__ == "__main__":
             rprint(f"  {i}: {ttype}")
 
         # apply sharding
-        payload = lower_payload(
-            payload,
-            wload.shard_schedule_modules(),
-        )
+        pipeline = TransformDriver(wload.shard_schedule_modules())
+        payload = pipeline.apply(payload)
 
         # inspect sharded payload function signature
         payload_metadata = inspect_payload(payload)
@@ -475,10 +473,8 @@ if __name__ == "__main__":
                 execution_engine.invoke("dealloc_2d", ptr_alloc)
 
         # execute once for correctness check
-        payload = lower_payload(
-            payload,
-            wload.schedule_modules(),
-        )
+        pipeline = pipeline = TransformDriver(wload.schedule_modules())
+        payload = pipeline.apply(payload)
         execute(
             payload,
             shared_libs=wload.shared_libs(),

lighthouse/execution/__init__.py
@@ -2,7 +2,6 @@ from .memory_manager import MemoryManager, GPUMemoryManager
 from .runner import (
     execute,
     benchmark,
-    lower_payload,
     get_bench_wrapper_schedule,
 )
 
@@ -12,5 +11,4 @@ __all__ = [
     "benchmark",
     "execute",
     "get_bench_wrapper_schedule",
-    "lower_payload",
 ]

lighthouse/execution/runner.py
@@ -17,7 +17,6 @@ from mlir.execution_engine import ExecutionEngine
 from mlir.runtime.np_to_memref import get_ranked_memref_descriptor
 
 from lighthouse.dialects import transform_ext
-from lighthouse.pipeline.driver import PipelineDriver
 from lighthouse.schedule import schedule_boilerplate
 from lighthouse.utils.memref import to_packed_args
 from lighthouse.utils.mlir import get_mlir_library_path
@@ -54,29 +53,6 @@ def get_engine(
     )
     execution_engine.initialize()
     return execution_engine
-
-
-def lower_payload(
-    payload_module: ir.Module,
-    schedule_modules: list[ir.Module],
-) -> ir.Module:
-    """
-    Apply transform schedules to the payload module.
-    Returns the lowered payload module.
-    FIXME: This should be removed and replaced with just using the PipelineDriver directly.
-    """
-    if not isinstance(schedule_modules, list):
-        raise TypeError(
-            f"schedule_modules() must return a list of ir.Module instances, "
-            f"got {type(schedule_modules).__name__}"
-        )
-    if not schedule_modules:
-        raise ValueError("schedule_modules() must return at least one schedule module.")
-
-    pipeline = PipelineDriver(payload_module.context)
-    for schedule_module in schedule_modules:
-        pipeline.add_transform(schedule_module)
-    return pipeline.apply(payload_module)
 
 
 def get_bench_wrapper_schedule(

commit 2dbe6966b9474f1ab30f1f3ea7eb9edc5f4df22e

[xegpu] Fix xegpu examples (#104)
Fix examples that were broken in #102

examples/xegpu/matmul.py
@@ -177,7 +177,7 @@ class XeGPUMatMul:
         return ["libmlir_levelzero_runtime.so"]
 
 
-def execute_and_check(mmul: XeGPUMatMul, params: dict, verbose: int = 0) -> bool:
+def execute_and_check(mmul: XeGPUMatMul, payload: ir.Module, verbose: int = 0) -> bool:
     """Execute the matmul kernel and check correctness of the result."""
 
     # Setup callback function to copy result from device to host.
@@ -190,8 +190,7 @@ def execute_and_check(mmul: XeGPUMatMul, params: dict, verbose: int = 0) -> bool
 
     # Execute kernel once.
     execute(
-        mmul.payload_module(),
-        schedule_modules=mmul.schedule_modules(parameters=params),
+        payload,
         host_input_buffers=mmul._initial_host_arrays,
         mem_manager_cls=mmul.memory_manager_class,
         shared_libs=mmul.shared_libs(),
@@ -232,12 +231,11 @@ def execute_and_check(mmul: XeGPUMatMul, params: dict, verbose: int = 0) -> bool
 
 
 def run_benchmark(
-    mmul: XeGPUMatMul, params: dict, nruns: int = 100, nwarmup: int = 10
+    mmul: XeGPUMatMul, payload_module: ir.Module, nruns: int = 100, nwarmup: int = 10
 ) -> np.ndarray:
     """Benchmark the matmul kernel and return array of execution times in seconds."""
     times = benchmark(
-        mmul.payload_module(),
-        schedule_modules=mmul.schedule_modules(parameters=params),
+        payload_module,
         host_input_buffers=mmul._initial_host_arrays,
         mem_manager_cls=mmul.memory_manager_class,
         shared_libs=mmul.shared_libs(),
@@ -462,24 +460,28 @@ enabled via CLI arguments.
         )
 
         if args.dump_kernel or args.dump_schedule:
-            pipeline = TransformDriver(
-                wload.schedule_modules(
-                    stop_at_stage=args.dump_kernel, parameters=params
-                )
-            )
-            payload = pipeline.apply(wload.payload_module())
             if args.dump_kernel:
+                pipeline = TransformDriver(
+                    wload.schedule_modules(
+                        stop_at_stage=args.dump_kernel, parameters=params
+                    )
+                )
+                payload = pipeline.apply(wload.payload_module())
                 print(payload)
             if args.dump_schedule:
                 for schedule_module in wload.schedule_modules(parameters=params):
                     print(schedule_module)
         else:
+            pipeline = TransformDriver(wload.schedule_modules(parameters=params))
+            payload = pipeline.apply(wload.payload_module())
             if args.check_result:
-                success = execute_and_check(wload, params, args.verbose)
+                success = execute_and_check(wload, payload, args.verbose)
                 if not success:
                     raise ValueError("Result mismatch!")
 
-            times = run_benchmark(wload, params, nruns=args.nruns, nwarmup=args.nwarmup)
+            times = run_benchmark(
+                wload, payload, nruns=args.nruns, nwarmup=args.nwarmup
+            )
             times *= 1e6  # convert to microseconds
             elapsed = np.mean(times)
             flop_count = wload.get_complexity()[0]

examples/xegpu/mlp.py
@@ -384,6 +384,8 @@ if __name__ == "__main__":
                 for schedule_module in wload.schedule_modules(parameters=params):
                     print(schedule_module)
         else:
+            pipeline = TransformDriver(wload.schedule_modules(parameters=params))
+            payload = pipeline.apply(wload.payload_module())
             if args.check_result:
                 # Setup callback function to copy result from device to host.
                 result_host_copy = np.zeros(wload.output_shape, dtype=wload.ab_dtype)
@@ -397,8 +399,6 @@ if __name__ == "__main__":
                     memory_manager.copy(inputs[0], result_host_copy)
 
                 # Execute kernel once.
-                pipeline = TransformDriver(wload.schedule_modules(parameters=params))
-                payload = pipeline.apply(wload.payload_module())
                 execute(
                     payload,
                     host_input_buffers=wload._initial_host_arrays,

examples/xegpu/softmax.py
@@ -259,6 +259,8 @@ if __name__ == "__main__":
                 for schedule_module in wload.schedule_modules(parameters=params):
                     print(schedule_module)
         else:
+            pipeline = TransformDriver(wload.schedule_modules(parameters=params))
+            payload = pipeline.apply(wload.payload_module())
             if args.check_result:
                 # Setup callback function to copy result from device to host.
                 result_host_copy = np.zeros(wload.shape, dtype=wload.dtype)
@@ -272,8 +274,6 @@ if __name__ == "__main__":
                     memory_manager.copy(inputs[0], result_host_copy)
 
                 # Execute kernel once.
-                pipeline = TransformDriver(wload.schedule_modules(parameters=params))
-                payload = pipeline.apply(wload.payload_module())
                 execute(
                     payload,
                     host_input_buffers=wload._initial_host_arrays,